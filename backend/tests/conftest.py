65896ccbc90cc3ef04e6ae6f176a4c8e6f99c803
lap2004 <lapthai03@gmail.com>
2026-06-01 15:09:04 +0700

    update cron-test-v6

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -8,11 +8,13 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from app.main import app
 
+import pytest_asyncio
+
 @pytest.fixture
 def anyio_backend():
     return "asyncio"
 
-@pytest.fixture
+@pytest_asyncio.fixture
 async def async_client():
     async with AsyncClient(
         transport=ASGITransport(app=app),
